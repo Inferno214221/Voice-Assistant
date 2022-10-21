aba5b5c486a88ce54ea93bb69c3a33acb43b513f
Inferno214221 <inferno214221@gmail.com>
2022-10-21 20:34:08 +1030

    v0.1
    Can run extensions from the correct directory. I should add more calls for the extension.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var vosk = require('vosk')
-
+#!/usr/bin/node
+const cwd = __dirname + "/";
+var vosk = require('vosk');
 const fs = require("fs");
 var mic = require("mic");
+const { dir } = require('console');
+const { isBoxedPrimitive } = require('util/types');
+const config = require(cwd + "extensions/config.json");
+const extensions = [];
+fs.readdirSync(cwd + "extensions").filter(function (file) {
+    return fs.statSync(cwd + "extensions/" + file).isDirectory();
+}).forEach(directory => {
+    extensions[directory] = require(cwd + "extensions/" + directory + "/index.js");
+});
 
-MODEL_PATH = "model"
+MODEL_PATH = cwd + "model"
 SAMPLE_RATE = 16000
 
 if (!fs.existsSync(MODEL_PATH)) {
@@ -13,7 +23,7 @@ if (!fs.existsSync(MODEL_PATH)) {
 
 vosk.setLogLevel(0);
 const model = new vosk.Model(MODEL_PATH);
-const rec = new vosk.Recognizer({model: model, sampleRate: SAMPLE_RATE});
+const rec = new vosk.Recognizer({ model: model, sampleRate: SAMPLE_RATE });
 
 var micInstance = mic({
     rate: String(SAMPLE_RATE),
@@ -25,22 +35,58 @@ var micInstance = mic({
 var micInputStream = micInstance.getAudioStream();
 
 micInputStream.on('data', data => {
-    if (rec.acceptWaveform(data))
-        console.log(rec.result());
-    else
-        console.log(rec.partialResult());
+    if (rec.acceptWaveform(data)) {
+        interpret(rec.result()["text"]);
+    }
 });
 
-micInputStream.on('audioProcessExitComplete', function() {
+micInputStream.on('audioProcessExitComplete', function () {
     console.log("Cleaning up");
     console.log(rec.finalResult());
     rec.free();
     model.free();
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
     console.log("\nStopping");
     micInstance.stop();
 })
 
 micInstance.start();
+
+function interpret(result) {
+    console.log(result);
+    var activate;
+    config["activate"].forEach(i => {
+        if (result.includes(i)) {
+            activate = i;
+        }
+    });
+    if (activate == undefined) {
+        console.log("Not activated");
+        return;
+    }
+    console.log("Activated");
+    result = result.replace(activate + " ", "");
+    console.log(result);
+    
+    var bestTrigger;
+    Object.keys(config["triggers"]).forEach(trigger => {
+        if (result.includes(trigger)) {
+            bestTrigger = trigger;
+            //TODO: Find the best trigger during conflicts
+        }
+    });
+    if (bestTrigger == undefined) {
+        console.log("No trigger found");
+        return;
+    }
+    console.log("Triggered");
+    if (result.includes(bestTrigger + " ")) {
+        result = result.replace(bestTrigger + " ", "");
+    } else { // Is it bad to run twice? If i said the name of the extension twice it should only remove it once.
+        result = result.replace(" " + bestTrigger, "");
+    }
+
+    extensions[config["triggers"][bestTrigger]].run(result);
+}
